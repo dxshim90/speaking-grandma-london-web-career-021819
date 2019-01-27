e880ea9e5f0cb21babe16d1c41d6aa0036407b84
Daniel Shimield <dxshim90@gmail.com>
2019-01-27 15:45:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.Capitalize! == false
+  if phrase.capitalize! == false
     return "HUH?! SPEAK UP SONNY!."
-    else if phrase.Capitalize! == true
+    else if phrase.capitalize! == true
       return "NO, NOT SINCE 1938!."
       else if phrase == "I LOVE YOU GRANDMA"
         return "I LOVE YOU TOO PUMPKIN!"
