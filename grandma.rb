c422c2343e66d331d5b90b906e082a024772988e
Daniel Shimield <dxshim90@gmail.com>
2019-01-27 16:00:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ if phrase == "I LOVE YOU GRANDMA"
   return "I LOVE YOU TOO PUMPKIN"
   elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.downcase
+else
   return "HUH?! SPEAK UP, SONNY!"
 end
 end
